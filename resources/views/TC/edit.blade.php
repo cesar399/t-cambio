7b049ca86655acbe798802381edf2b5f6834357b
Cesar Reyes <60906015+cesar399@users.noreply.github.com>
2021-08-14 04:49:47 -0500

    tc form to TC form

--- resources/views/TC/edit.blade.php
+++ resources/views/TC/edit.blade.php
@@ -8,7 +8,7 @@
         <x-auth-card>
             <form method="POST" action="{{route('tipo_cambio.update', $tipo_cambio->id)}}" enctype="multipart/form-data">
                 @method('PUT')
-                @include('tc.partials.form')
+                @include('TC.partials.form')
             </form>
         </x-auth-card>
     </div>
